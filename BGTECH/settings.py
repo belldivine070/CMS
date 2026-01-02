426e12f411acd05fb0ee79a5559b491c04d85eb8
belldivine070 <belldivine070@gmail.com>
2026-01-02 09:21:22 +0100

    Change DEBUG setting to True for development

--- BGTECH/settings.py
+++ BGTECH/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7+ok!u*d0s#n!ida$djxkjm-*=u4_r822)(&l+qkm-$ezx9h*g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
