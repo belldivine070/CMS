667ee9cdee4fe9f6d51eeaff6ebb1bf231e762bc
belldivine070 <belldivine070@gmail.com>
2026-01-06 13:49:27 +0100

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(
         template_name='registration/login.html', 
         authentication_form=EmailAuthenticationForm,
-        next_page='index' 
+        next_page='users:index' 
     ), name='login'), 
     path('logout/', LogoutView.as_view(next_page='users:login'), name='logout'),
     path('password/change/', views.CustomPasswordChangeView.as_view(), name='password_change'),
